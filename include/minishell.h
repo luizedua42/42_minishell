373d030cfa241244b7d312789617b5242602889e
luizedua42 <luizedua@student.42sp.org.br>
2023-09-16 19:38:37 -0300

    first steps

--- include/minishell.h
+++ include/minishell.h
@@ -1,6 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 17:01:33 by luizedua          #+#    #+#             */
+/*   Updated: 2023/09/16 18:04:28 by luizedua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
+# include "libft.h" 
+
+typedef enum e_token_type
+{
+	AND,
+	OR,
+	PIPE,
+	WORD,
+	REDIRECT_IN,
+	REDIRECT_OUT,
+	HEREDOC_IN,
+	HEREDOC_OUT,
+	QUOTE,
+	DQUOTE,
+	DOLLAR_SIGN,
+	OPEN_PARENTHESIS,
+	CLOSE_PARENTHESIS,
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type type;
+	char	*value;	
+}	t_token;
 
 #endif
--- src/mandatory/minishell.c
+++ src/mandatory/minishell.c
@@ -1,6 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 17:01:24 by luizedua          #+#    #+#             */
+/*   Updated: 2023/09/16 19:35:31 by luizedua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int main(void)
+t_token	*new_token(t_token_type type, char * value)
 {
-  return (0);
+	t_token *token;
+	token = ft_calloc(1, sizeof(t_token));
+	token->type = type;
+	token->value = value;
+	return (token);
+}
+
+int main(int argc, char **argv)
+{
+	t_list *tokens;
+	t_token *teste;
+
+	int i = 0;
+	if (argc != 2)
+		return 1;
+	tokens  = NULL;
+	while(argv[1][i])
+	{
+		if(ft_strncmp(&argv[1][i],"|",1) == 0)
+			ft_lstadd_back(&tokens, ft_lstnew(new_token(PIPE, "|")));
+		else if (ft_strncmp(&argv[1][i],"&&",1) == 0)
+			ft_lstadd_back(&tokens, ft_lstnew(new_token(AND, "&&")));
+		else if (ft_strncmp(&argv[1][i],"&&",1) == 0)
+			ft_lstadd_back(&tokens, ft_lstnew(new_token(AND, "&&")));
+		else if (ft_strncmp(&argv[1][i],"&&",1) == 0)
+			ft_lstadd_back(&tokens, ft_lstnew(new_token(AND, "&&")));
+		else if (ft_strncmp(&argv[1][i],"&&",1) == 0)
+			ft_lstadd_back(&tokens, ft_lstnew(new_token(AND, "&&")));
+		else if (ft_strncmp(&argv[1][i],"&&",1) == 0)
+			ft_lstadd_back(&tokens, ft_lstnew(new_token(AND, "&&")));
+		else if (ft_strncmp(&argv[1][i],"&&",1) == 0)
+			ft_lstadd_back(&tokens, ft_lstnew(new_token(AND, "&&")));
+		i++;
+	}
+	teste = (t_token*)(tokens->content);
+	ft_printf("%s", teste->value);
+	return (0);
 }
